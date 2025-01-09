a3b6e726a905aa2a033066ff7d558c65caa19d38
Adityamewmew <purnamaaditya939@gmail.com>
2025-01-09 17:20:23 +0700

    Menambahkan welcome.dart untuk tampilan awal

---
+++ lib/Screens/welcome.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+
+class WelcomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset('assets/images/cumipakkris.jpeg', height: 200, width: 200),
+            SizedBox(height: 20),
+            Text(
+              'Welcome to Learnout',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => SignInScreen()),
+                );
+              },
+              child: Text('Sign In'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => SignUpScreen()),
+                );
+              },
+              child: Text('Sign Up'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
